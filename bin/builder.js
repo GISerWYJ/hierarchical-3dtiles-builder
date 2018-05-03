dc0dd2a65d982902c3fcad7712622f9bbee4d291
CrashedBboy <crashedbboy@gmail.com>
2018-05-03 14:36:23 +0800

    add yargs dependency, setup args

--- bin/builder.js
+++ bin/builder.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('helllo builder');
+var argv = require('yargs')
+    .usage('\nUsage: $0 --setting path --output directory')
+    .alias('h', 'help')
+    .help()
+    .options({
+        setting: {
+            alias: 's',
+            describe: 'Path to the setting file',
+            type: 'string',
+            normalize: true,
+            demandOption: true
+        },
+        output: {
+            alias: 'o',
+            describe: 'Directory path for output',
+            type: 'string',
+            normalize: true,
+            demandOption: true
+        }
+    })
+    .parse();
+
+console.log(argv);
+
